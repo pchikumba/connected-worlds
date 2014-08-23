77aaf65e76a789ea38f2b8f6f62ba4966fbb6f95
Rob Bell <me@rob-bell.net>
2014-08-23 10:35:57 +0100

    Adding LibGDX-generated projects

---
+++ desktop/src/com/robbell/connectedworlds/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.robbell.connectedworlds.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.robbell.connectedworlds.ConnectedWorldsGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ConnectedWorldsGame(), config);
+	}
+}
